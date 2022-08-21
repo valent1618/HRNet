4c83fe5d8f913eca74860ffccf525c8e295c09c8
valent1618 <valentinlemaire1618@gmail.com>
2022-08-21 10:56:20 +0200

    Add Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './app/store';
+
+import './styles/main.scss';
+
+// Layouts
+import Navigation from './layouts/Navigation';
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
+
+// Pages
+import Error404 from './pages/404';
+import Create from './pages/Create';
+import List from './pages/List';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={store}>
+        <Header />
+        <Navigation />
+        <Routes>
+          <Route path='/create' element={<Create />} />
+          <Route path='/list' element={<List />} />
+          <Route path='*' element={<Error404 />} />
+        </Routes>
+        <Footer />
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
 
