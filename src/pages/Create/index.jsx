1c87571b317479704ea86ca72615f46fdfc9524d
valent1618 <valentinlemaire1618@gmail.com>
2022-08-21 10:56:35 +0200

    add page Create

---
+++ src/pages/Create/index.jsx
@@ -0,0 +1,53 @@
+import PATTERN_NAME from '../../utils/patternName';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
+import STATES from '../../data/states';
+import DEPARTMENTS from '../../data/departments';
+
+function Create() {
+  return (
+    <main id='Create'>
+      <h2>Create Employee</h2>
+      <form id='create-employee' autoComplete='off'>
+        <div className='flex-input'>
+          <Input
+            name='first-name'
+            label='First Name'
+            type='text'
+            pattern={PATTERN_NAME}
+          />
+          <Input
+            name='last-name'
+            label='Last Name'
+            type='text'
+            pattern={PATTERN_NAME}
+          />
+        </div>
+
+        <div className='flex-input'>
+          <Input name='date-of-birth' label='Date of Birth' type='date' />
+          <Input name='start-date' label='Start Date' type='date' />
+        </div>
+
+        <fieldset className='address'>
+          <legend>Address</legend>
+
+          <Input name='street' label='Street' type='text' />
+
+          <Input name='city' label='City' type='text' />
+
+          <Input name='zip-code' label='Zip Code' type='number' />
+
+          <Select name='state' options={STATES} />
+        </fieldset>
+
+        <Select name='department' options={DEPARTMENTS} />
+
+        <button>Create Employee</button>
+      </form>
+    </main>
+  );
+}
+
+export default Create;
