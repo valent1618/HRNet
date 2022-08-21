6aae5becf2da4134182588d422f5ebff08cc3b1c
valent1618 <valentinlemaire1618@gmail.com>
2022-08-21 10:55:16 +0200

    add layout Header

---
+++ src/layouts/Header/index.jsx
@@ -0,0 +1,14 @@
+import logo from '../../assets/logo.png';
+
+function Header() {
+  return (
+    <header id='Header'>
+      <img src={logo} alt='Logo' />
+      <h1>
+        <span>HR</span>Net
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
