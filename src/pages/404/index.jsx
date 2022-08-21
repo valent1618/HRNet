fdb8b4d65fb60c0241d0e66bc3fbec0a1f0ab940
valent1618 <valentinlemaire1618@gmail.com>
2022-08-21 10:56:26 +0200

    add page 404

---
+++ src/pages/404/index.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function Error404() {
+  return (
+    <main id='Error404'>
+      <p className='deco404'>404</p>
+      <h1>
+        Error <span>404</span>
+      </h1>
+      <h2>Page Not Found</h2>
+      <Link to='/create'>Return to create page</Link>
+    </main>
+  );
+}
+
+export default Error404;
