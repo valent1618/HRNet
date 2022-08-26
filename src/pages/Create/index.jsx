2d3c170c50ba8d2aaf3bfeb5504f3a0eba0b7d17
valent1618 <valentinlemaire1618@gmail.com>
2022-08-26 19:11:31 +0200

    add modal and handle it

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import { useStore } from 'react-redux';
 
 import PATTERN_NAME from '../../utils/patternName';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Modal from '../../components/Modal';
 
 import STATES from '../../data/states';
 import DEPARTMENTS from '../../data/departments';
@@ -11,14 +13,15 @@ import { createEmployee } from '../../features/employees';
 
 function Create() {
   const store = useStore();
+  const [modalText, setModalText] = useState('Employee created !');
 
   return (
     <main id='Create'>
-      <h2>Create Employee</h2>
+      <h2 className='glass-container'>Create Employee</h2>
       <form
         id='create-employee'
-        autoComplete='off'
-        onSubmit={(e) => createEmployee(e, store)}
+        onSubmit={(e) => createEmployee(e, store, setModalText)}
+        className='glass-container'
       >
         <div className='flex-input'>
           <Input
@@ -45,17 +48,21 @@ function Create() {
 
           <Input name='street' label='Street' type='text' />
 
-          <Input name='city' label='City' type='text' />
+          <Input name='city' label='City' type='text' maxLength={30} />
 
-          <Input name='zip-code' label='Zip Code' type='number' />
+          <Input name='zip-code' label='Zip Code' type='number' max={99950} />
 
-          <Select name='state' options={STATES} />
+          <Select name='state' options={Object.values(STATES)} />
         </fieldset>
 
         <Select name='department' options={DEPARTMENTS} />
 
         <button>Create Employee</button>
       </form>
+      <Modal
+        text={modalText}
+        link={{ path: '/list', text: 'View current employees' }}
+      />
     </main>
   );
 }
