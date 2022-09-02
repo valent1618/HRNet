dde4bbebd5ea0a99bf392718a2acc846dbdc9a78
valent1618 <valentinlemaire1618@gmail.com>
2022-09-02 17:40:01 +0200

    create array for table header

---
+++ src/data/tableHeader.js
@@ -0,0 +1,13 @@
+const tableHeader = [
+  'First Name',
+  'Last Name',
+  'Start Date',
+  'Departement',
+  'Date of Birth',
+  'Street',
+  'City',
+  'State',
+  'Zip Code',
+];
+
+export default tableHeader;
