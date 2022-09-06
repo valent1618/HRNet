68774c2219c58cc16d3f2bbdc8367d90abd4a13c
valent1618 <valentinlemaire1618@gmail.com>
2022-09-06 19:18:24 +0200

    Allow to remove employee

--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -1,18 +1,38 @@
-import { useStore } from 'react-redux';
+import { useStore, useSelector } from 'react-redux';
+import { employees } from '../../app/selectors';
+import { useState, useEffect } from 'react';
 
 import tableHeader from '../../data/tableHeader';
 import Table from 'plaid-table';
+import Modal from '../../components/Modal';
+
+import eventRemove from './eventRemove';
+import { removeEmployee } from '../../features/employees';
+import { closeModal } from '../../utils/handleModal';
 
 function List() {
   const store = useStore();
-  const employees = store.getState().employees;
+  const employeesRedux = useSelector(employees);
+
+  const [toRemove, setToRemove] = useState('');
+
+  useEffect(() => {
+    employeesRedux.length > 0 && eventRemove(setToRemove);
+  }, [employeesRedux]);
 
   return (
     <main id='List'>
       <Table
-        title={'Current employees'}
+        title='Current employees'
         headers={tableHeader}
-        body={employees}
+        body={employeesRedux}
+      />
+      <Modal
+        text={`Do you want to delete ${toRemove.firstName} ?`}
+        buttons={[
+          { action: () => removeEmployee(toRemove, store), text: 'Yes' },
+          { action: () => closeModal(), text: 'No' },
+        ]}
       />
     </main>
   );
