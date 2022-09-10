d31247f6646aba31750b8b00c5ff3f4fac93ed2e
valent1618 <valentinlemaire1618@gmail.com>
2022-09-10 12:11:45 +0200

    add options required and errorMessage

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -3,32 +3,46 @@ import PropTypes from 'prop-types';
 /**
  * Custom input that shows you if it is valid or not
  * @component
- * @param { {name: string, label: string, type: ('date' | 'datetime-local' | 'email' | 'month' | 'number' | 'password' | 'search' | 'tel' | 'text' | 'time' | 'url' | 'week'), pattern: string, minLength: 3, maxLength: 50, min: 0, max: 1000000} } Props
+ * @param { {name: string, label: string, type: ('date' | 'datetime-local' | 'email' | 'month' | 'number' | 'password' | 'search' | 'tel' | 'text' | 'time' | 'url' | 'week'), required: true, pattern: string, errorMessage: string, minLength: 3, maxLength: 50, min: 0, max: 1000000} } Props
  * @returns
  */
 function Input({
   name,
   label,
   type,
+  required = true,
   pattern,
+  errorMessage,
   minLength = 3,
   maxLength = 50,
   min = 0,
   max = 1000000,
 }) {
   return (
-    <div className={`input-container ${name}`}>
+    <div className={`input-container ${name}`} data-testid='container'>
       <input
         id={name}
+        name={name}
         type={type}
-        required
+        required={required}
         pattern={pattern}
         minLength={minLength}
         maxLength={maxLength}
         min={type === 'number' ? min : undefined}
         max={type === 'number' ? max : undefined}
+        data-testid='input'
+        onInvalid={
+          errorMessage
+            ? (e) => e.target.setCustomValidity(errorMessage)
+            : undefined
+        }
+        onInput={
+          errorMessage ? (e) => e.target.setCustomValidity('') : undefined
+        }
       />
-      <label htmlFor={name}>{label}</label>
+      <label htmlFor={name} data-testid='label'>
+        {label}
+      </label>
     </div>
   );
 }
@@ -52,7 +66,9 @@ Input.propTypes = {
     'url',
     'week',
   ]),
+  required: PropTypes.bool,
   pattern: PropTypes.string,
+  errorMessage: PropTypes.string,
   minLength: PropTypes.number,
   maxLength: PropTypes.number,
   min: PropTypes.number,
