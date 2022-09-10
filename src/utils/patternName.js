5f02caa039e04839713caff9b08ac27aec933db1
valent1618 <valentinlemaire1618@gmail.com>
2022-09-10 12:12:45 +0200

    transform to object for add message

--- src/utils/patternName.js
+++ src/utils/patternName.js
@@ -1,7 +1,10 @@
 /**
  * Regex for name, accepts compound names, accents, leading capitals only
  */
-const PATTERN_NAME =
-  '([A-Z]{1})?[a-zÜ-ü]{2,24}([ -]{1}([A-Z]{1})?[a-zÜ-ü]{1,24})?';
+const PATTERN_NAME = {
+  regex: '([A-Z]{1})?[a-zÜ-ü]{2,24}([ -]{1}([A-Z]{1})?[a-zÜ-ü]{1,24})?',
+  message:
+    'Accepts compound names (space or -), accents and leading capitals. Special characters are not allowed.',
+};
 
 export default PATTERN_NAME;
