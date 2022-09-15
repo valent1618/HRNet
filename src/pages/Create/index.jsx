24228e3448ece60c3128d2344e17e96b3f94b39d
valent1618 <valentinlemaire1618@gmail.com>
2022-09-15 18:54:23 +0200

    Add more requirements for inputs

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -42,18 +42,47 @@ function Create() {
         </div>
 
         <div className='flex-input'>
-          <Input name='date-of-birth' label='Date of Birth' type='date' />
-          <Input name='start-date' label='Start Date' type='date' />
+          <Input
+            name='date-of-birth'
+            label='Date of Birth'
+            type='date'
+            min={'1900-01-01'}
+            max={'2006-01-01'}
+          />
+          <Input
+            name='start-date'
+            label='Start Date'
+            type='date'
+            min={'2020-01-01'}
+          />
         </div>
 
         <fieldset className='address'>
           <legend>Address</legend>
 
-          <Input name='street' label='Street' type='text' />
+          <Input
+            name='street'
+            label='Street'
+            type='text'
+            minLength={5}
+            maxLength={70}
+          />
 
-          <Input name='city' label='City' type='text' maxLength={30} />
+          <Input
+            name='city'
+            label='City'
+            type='text'
+            minLength={2}
+            maxLength={30}
+          />
 
-          <Input name='zip-code' label='Zip Code' type='number' max={99950} />
+          <Input
+            name='zip-code'
+            label='Zip Code'
+            type='number'
+            min={1}
+            max={99950}
+          />
 
           <Select name='state' options={Object.values(STATES)} />
         </fieldset>
