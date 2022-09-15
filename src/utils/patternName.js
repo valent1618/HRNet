cdf06e046ff8ab3cbfb74ce827198accd12b1bf1
valent1618 <valentinlemaire1618@gmail.com>
2022-09-15 18:56:42 +0200

    Accept uppercase

--- src/utils/patternName.js
+++ src/utils/patternName.js
@@ -1,8 +1,8 @@
 /**
- * Regex for name, accepts compound names, accents, leading capitals only
+ * Regex for name and accepts compound names, accents
  */
 const PATTERN_NAME = {
-  regex: '([A-Z]{1})?[a-zÜ-ü]{2,24}([ -]{1}([A-Z]{1})?[a-zÜ-ü]{1,24})?',
+  regex: '[A-zÜ-ü]{2,24}([ -]{1}[A-zÜ-ü]{1,24})?',
   message:
     'Accepts compound names (space or -), accents and leading capitals. Special characters are not allowed.',
 };
