9d072cfe8471da4f8f89e3628b2f4557b4d02616
valent1618 <valentinlemaire1618@gmail.com>
2022-09-16 11:08:48 +0200

    Change message and doc

--- src/utils/patternName.js
+++ src/utils/patternName.js
@@ -1,10 +1,10 @@
 /**
- * Regex for name and accepts compound names, accents
+ * Regex for name, accepts compound names and accents
  */
 const PATTERN_NAME = {
   regex: '[A-zÜ-ü]{2,24}([ -]{1}[A-zÜ-ü]{1,24})?',
   message:
-    'Accepts compound names (space or -), accents and leading capitals. Special characters are not allowed.',
+    'Accepts compound names (space or -) and accents. Special characters are not allowed.',
 };
 
 export default PATTERN_NAME;
